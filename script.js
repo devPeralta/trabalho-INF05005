f09be581a8b5c71c01e3914f726cb66963959595
Pablo Peralta <pablodiedrich@hotmail.com>
2024-02-02 10:50:53 -0300

    primeiros 3 turnos prontos

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
 let posStatus = [9];
 let posTag = [9];
-let matrizGame = [9];
+let botPlays = [5];
 let rotateStatus = 0; // 0 = sem rotate / 1 = rotate right / 2 = rotate rotate / 3 = rotate left
+
+const Rotate = {
+    0: "normal",
+    1: "right",
+    2: "duplo",
+    3: "left"
+};
 
 posTag[0] = da1;
 posTag[1] = la1;
@@ -14,11 +21,11 @@ posTag[6] = db2;
 posTag[7] = lb1;
 posTag[8] = da2;
 
-for(let i=0;i<9;i++){
+for (let i = 0; i < 9; i++) {
     posStatus[i] = 0;
 }
 
-function rotateL(vet){
+function rotateL(vet) {
 
     let vetL = vet.slice();
 
@@ -32,27 +39,23 @@ function rotateL(vet){
     vetL[5] = vet[7];
     vetL[2] = vet[8];
 
-    if(rotateStatus == 0){
+    if (rotateStatus == 0) {
         rotateStatus = 3;
-        console.log("normal -> esq");
     }
-    else if(rotateStatus == 1){
+    else if (rotateStatus == 1) {
         rotateStatus = 0;
-        console.log("dir -> normal");
     }
-    else if(rotateStatus == 2){
+    else if (rotateStatus == 2) {
         rotateStatus = 1;
-        console.log("duplo -> dir");
     }
-    else if(rotateStatus == 3){
+    else if (rotateStatus == 3) {
         rotateStatus = 2;
-        console.log("esq -> duplo");
     }
-    
+
     return vetL;
 }
 
-function rotateR(vet){
+function rotateR(vet) {
 
     let vetR = vet.slice();
 
@@ -66,33 +69,192 @@ function rotateR(vet){
     vetR[7] = vet[5];
     vetR[8] = vet[2];
 
-    if(rotateStatus == 0){
+    if (rotateStatus == 0) {
         rotateStatus = 1;
-        console.log("normal -> dir");
     }
-    else if(rotateStatus == 1){
+    else if (rotateStatus == 1) {
         rotateStatus = 2;
-        console.log("dir -> duplo");
     }
-    else if(rotateStatus == 2){
+    else if (rotateStatus == 2) {
         rotateStatus = 3;
-        console.log("duplo -> esq");
     }
-    else{
+    else {
         rotateStatus = 0;
-        console.log("esq -> normal");
     }
 
     return vetR;
 }
 
+function rotateNum(x){
+    switch(x){
+        case 0:
+            switch(rotateStatus){
+                case 0:
+                break;
+                case 1:
+                    x = 6;
+                break;
+                case 2:
+                    x = 8;
+                break;
+                case 3:
+                    x = 2;
+                break;
+                default:
+                console.clear();
+                console.log("Erro no rotateNum: input(" + x + ")");
+            }
+            break;
+        case 1:
+            switch(rotateStatus){
+            case 0:
+            break;
+            case 1:
+                x = 3;
+            break;
+            case 2:
+                x = 7;
+            break;
+            case 3:
+                x = 5;
+            break;
+            default:
+            console.clear();
+            console.log("Erro no rotateNum: input(" + x + ")");
+        }
+        break;
+        case 2:
+            switch(rotateStatus){
+            case 0:
+            break;
+            case 1:
+                x = 0;
+            break;
+            case 2:
+                x = 6;
+            break;
+            case 3:
+                x = 8;
+            break;
+            default:
+            console.clear();
+            console.log("Erro no rotateNum: input(" + x + ")");
+        }
+            break;
+        case 3:
+            switch(rotateStatus){
+            case 0:
+            break;
+            case 1:
+                x = 7;
+            break;
+            case 2:
+                x = 5;
+            break;
+            case 3:
+                x = 1;
+            break;
+            default:
+            console.clear();
+            console.log("Erro no rotateNum: input(" + x + ")");
+        }
+            break;
+        case 4:{
 
+        }
+            break;
+        case 5:
+            switch(rotateStatus){
+            case 0:
+            break;
+            case 1:
+                x = 1;
+            break;
+            case 2:
+                x = 3;
+            break;
+            case 3:
+                x = 7;
+            break;
+            default:
+            console.clear();
+            console.log("Erro no rotateNum: input(" + x + ")");
+        }
+            break;
+        case 6:
+            switch(rotateStatus){
+            case 0:
+            break;
+            case 1:
+                x = 8;
+            break;
+            case 2:
+                x = 2;
+            break;
+            case 3:
+                x = 0;
+            break;
+            default:
+            console.clear();
+            console.log("Erro no rotateNum: input(" + x + ")");
+        }
+            break;
+        case 7:
+            switch(rotateStatus){
+            case 0:
+            break;
+            case 1:
+                x = 5;
+            break;
+            case 2:
+                x = 1;
+            break;
+            case 3:
+                x = 3;
+            break;
+            default:
+            console.clear();
+            console.log("Erro no rotateNum: input(" + x + ")");
+        }
+            break;
+        case 8:
+            switch(rotateStatus){
+            case 0:
+            break;
+            case 1:
+                x = 2;
+            break;
+            case 2:
+                x = 0;
+            break;
+            case 3:
+                x = 6;
+            break;
+            default:
+            console.clear();
+            console.log("Erro no rotateNum: input(" + x + ")");
+        }
+            break;
+        default:
+        console.clear();
+        console.log("Erro no rotateX (x)");
+    }
 
-function turnCount(){
-    let counter=0;
+    return x;
+}
+
+function insereX(posTela) {
+
+    posTela = rotateNum(posTela);
+
+    posTag[posTela].style.backgroundImage = "url('cross2.png')";
+}
+
+function turnCount() {
+    let counter = 0;
 
     for (let i = 0; i < 9; i++) {
-        if(posStatus[i] == 1){
+        if (posStatus[i] == 1) {
             counter++;
         }
     }
@@ -100,84 +262,138 @@ function turnCount(){
     return counter;
 }
 
-function playBot(){
-    let posBotTela = Math.floor(Math.random() * 9);
-
-    if(turnCount()==0){
-        if(posBotTela == 0 || posBotTela == 1 || posBotTela == 4){
-            posTag[posBotTela].style.backgroundImage = "url('cross2.png')";
+function playBot() {
+    //TURNO 1 ##################################################################
+    if (turnCount() == 0) {
+        let posBotTela = Math.floor(Math.random() * 9);
+        //let posBotTela = 4;
+        if (posBotTela == 0 || posBotTela == 1 || posBotTela == 4) {
+            insereX(posBotTela);
             posStatus[posBotTela] = 1;
         }
-        else if(posBotTela == 2 || posBotTela == 5){
-            posTag[posBotTela].style.backgroundImage = "url('cross2.png')";
+        else if (posBotTela == 2 || posBotTela == 5) {
+            insereX(posBotTela);
             posStatus[posBotTela] = 1;
-            posStatus = rotateL(posStatus,rotateStatus);
-            console.log(posStatus);
+            posStatus = rotateL(posStatus, rotateStatus);
         }
-        else if(posBotTela == 6 || posBotTela == 3){
-            posTag[posBotTela].style.backgroundImage = "url('cross2.png')";
+        else if (posBotTela == 6 || posBotTela == 3) {
+            insereX(posBotTela);
             posStatus[posBotTela] = 1;
-            posStatus = rotateR(posStatus,rotateStatus);
-            console.log(posStatus);
+            posStatus = rotateR(posStatus, rotateStatus);
+        }
+        else {
+            insereX(posBotTela);
+            posStatus[posBotTela] = 1;
+            posStatus = rotateR(rotateR(posStatus, rotateStatus), rotateStatus);
+        }
+        botPlays[0] = posBotTela;
+    }
+    //TURNO 3 ##################################################################
+    else if (turnCount() == 2) {
+    //Q5
+        //nao houve rotate
+        if (posStatus[0] == 1 && posStatus[4] == 1 && botPlays[0] == 4) {
+            insereX(2);
+            posStatus[2] = 1;
+            console.log("1");
+        }
+        //houve rotate right
+        else if (posStatus[2] == 1 && posStatus[4] == 1 && botPlays[0] == 4) {
+            posStatus = rotateL(posStatus, rotateStatus);
+            insereX(2);
+            posStatus[2] = 1;
+        }
+        //houve rotate left
+        else if (posStatus[6] == 1 && posStatus[4] == 1 && botPlays[0] == 4) {
+            posStatus = rotateR(posStatus, rotateStatus);
+            insereX(2);
+            posStatus[2] = 1;
+        }
+        //houve rotate duplo
+        else if (posStatus[8] == 1 && posStatus[4] == 1 && botPlays[0] == 4) {
+            posStatus = rotateR(rotateR(posStatus, rotateStatus), rotateStatus);
+            insereX(2);
+            posStatus[2] = 1;
+        }
+    //Q21
+        else if (posStatus[0] == 1 && posStatus[4] == 1) {
+           insereX(8);
+           posStatus[8] = 1;
+        }
+    //Q14 ou Q15
+        else if (posStatus[1] == 1 && posStatus[4] == 1 || (posStatus[1] == 1 && posStatus[7] == 1))  {
+            insereX(0);
+            posStatus[0] = 1;
+        }
+        else if ((posStatus[1] == 1 && posStatus[3] == 1) || (posStatus[1] == 1 && posStatus[5] == 1)) {
+            console.log("PERDEU O JOGO");
+            // TO DO
         }
         else{
-            posTag[posBotTela].style.backgroundImage = "url('cross2.png')";
-            posStatus[posBotTela] = 1;
-            posStatus = rotateR(rotateR(posStatus,rotateStatus),rotateStatus); 
-            console.log(posStatus);
+            insereX(4);
+            posStatus[4] = 1;
         }
-
-        console.log("rotate status: "+rotateStatus);
     }
-    else if(turnCount() == 2){
+    //TURNO 3 ##################################################################
+    else if(turnCount() == 4){
         
     }
 }
 
-function clicked(posClick){
+function clicked(posClick) {
 
     let clickDetect = 0;
 
     for (let i = 0; i < 9; i++) {
-        if (rotateStatus == 0) {
-            if (posClick == i && posStatus[i] == 0) {
-                posTag[i].style.backgroundImage = "url('circulo.png')";
-                posStatus[i] = 1;
-                clickDetect = 1;
-                console.log("rot0 = " + posStatus);
-            }
-        }
-        else if(rotateStatus == 1){
-            posStatus = rotateL(posStatus,rotateStatus); 
-            if (posClick == i && posStatus[i] == 0) {
-                posTag[i].style.backgroundImage = "url('circulo.png')";
-                posStatus[i] = 1;
-                clickDetect = 1;
-            }
-            posStatus = rotateR(posStatus,rotateStatus); 
-            console.log("rot1 = " + posStatus);
-        }
-        else if(rotateStatus == 2){
-            posStatus = rotateL(rotateL(posStatus,rotateStatus),rotateStatus); 
-            if (posClick == i && posStatus[i] == 0) {
-                posTag[i].style.backgroundImage = "url('circulo.png')";
-                posStatus[i] = 1;
-                clickDetect = 1;
-            }
-            posStatus = rotateL(rotateL(posStatus,rotateStatus),rotateStatus);
-            console.log("rot2 = " + posStatus);
-        }
-        else if(rotateStatus == 3){ 
-            posStatus = rotateR(posStatus,rotateStatus);
-            if (posClick == i && posStatus[i] == 0) {
-                posTag[i].style.backgroundImage = "url('circulo.png')";
-                posStatus[i] = 1;
-                clickDetect = 1;
-            }
-            posStatus = rotateL(posStatus,rotateStatus);
-            console.log("rot3 = " + posStatus);
-        }
+        switch (rotateStatus) {
 
+
+            case 0: {
+                if (posClick == i && posStatus[i] == 0) {
+                    posTag[i].style.backgroundImage = "url('circulo.png')";
+                    posStatus[i] = 1;
+                    clickDetect = 1;
+                    console.log("rot0 = " + posStatus);
+                }
+            }
+                break;
+            case 1: {
+                posStatus = rotateL(posStatus, rotateStatus);
+                if (posClick == i && posStatus[i] == 0) {
+                    posTag[i].style.backgroundImage = "url('circulo.png')";
+                    posStatus[i] = 1;
+                    clickDetect = 1;
+                }
+                posStatus = rotateR(posStatus, rotateStatus);
+                console.log("rot1 = " + posStatus);
+            }
+                break;
+            case 2: {
+                posStatus = rotateL(rotateL(posStatus, rotateStatus), rotateStatus);
+                if (posClick == i && posStatus[i] == 0) {
+                    posTag[i].style.backgroundImage = "url('circulo.png')";
+                    posStatus[i] = 1;
+                    clickDetect = 1;
+                }
+                posStatus = rotateL(rotateL(posStatus, rotateStatus), rotateStatus);
+                console.log("rot2 = " + posStatus);
+            }
+                break;
+            case 3: {
+                posStatus = rotateR(posStatus, rotateStatus);
+                if (posClick == i && posStatus[i] == 0) {
+                    posTag[i].style.backgroundImage = "url('circulo.png')";
+                    posStatus[i] = 1;
+                    clickDetect = 1;
+                }
+                posStatus = rotateL(posStatus, rotateStatus);
+                console.log("rot3 = " + posStatus);
+            }
+            break;
+            default:
+                console.log(ërro);
+
+        }
         if (clickDetect == 1) {
             console.log(posStatus)
             playBot();
